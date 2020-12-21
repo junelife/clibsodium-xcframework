7d07eaac8f6fc7ad3248b1a8c601e0c098c552dc
Joseph Ross <joe@junelife.com>
2020-12-21 11:56:24 -0800

    Just Clibsodium

--- Package.swift
+++ Package.swift
@@ -2,27 +2,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Sodium",
+    name: "Clibsodium",
     products: [
         .library(
             name: "Clibsodium",
             targets: ["Clibsodium"]),
-        .library(
-            name: "Sodium",
-            targets: ["Sodium"]),
     ],
     targets: [
         .binaryTarget(
             name: "Clibsodium",
             path: "Clibsodium.xcframework"),
-        .target(
-            name: "Sodium",
-            dependencies: ["Clibsodium"],
-            path: "Sodium",
-            exclude: ["libsodium", "Info.plist"]),
-        .testTarget(
-            name: "SodiumTests",
-            dependencies: ["Sodium"],
-            exclude: ["Info.plist"]),
     ]
 )
